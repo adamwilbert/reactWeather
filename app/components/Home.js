6d9b173f3c048d9537af80d3113beca3087bb6d4
Adam Wilbert <adam.wilbert@gmail.com>
2016-03-29 08:47:23 -0700

    adding initial routing, home component

---
+++ app/components/Home.js
@@ -0,0 +1,14 @@
+var React = require('react')
+var ReactRouter = require('react-router')
+var MainContainer = require('./MainContainer')
+
+var Home = React.createClass({
+  render: function () {
+    return (
+      <MainContainer>
+      </MainContainer>
+    )
+  }
+})
+
+module.exports = Home
