905d11a399f94414017a197ac82a86defbd3fa46
Adam Wilbert <adam.wilbert@gmail.com>
2016-04-04 19:49:08 -0700

    header element

--- app/components/MainContainer.js
+++ app/components/MainContainer.js
@@ -3,7 +3,13 @@ var styles = require('../styles')
 
 function MainContainer (props) {
   return (
+
     <div style={styles.bg} className='container' >
+      <div>
+        <h1>
+        WHETHER
+        </h1>
+      </div>
     {props.children}
     </div>
   )
